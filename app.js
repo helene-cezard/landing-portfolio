7172b14f8cddce3e92b9b30d3842ce8d4464029c
helene-cezard <helene.cezard@gmail.com>
2025-06-06 22:28:13 +0200

    info in files

--- app.js
+++ app.js
@@ -24,6 +24,107 @@ function init() {
       navbarLink.addEventListener("click", toggleMenu);
     }
   )
+
+  
+  async function createAboutMe() {
+    
+    const aboutMeText = await fetch("files/aboutMe.txt")
+      .then(response => response.text())
+      .then(text => text);
+    
+    const aboutMeArray = aboutMeText.split("\n");
+    
+    const about = document.getElementById("about");
+    aboutMeArray.forEach(
+      function(paragraph) {
+        const newParagraph = document.createElement("p");
+        newParagraph.textContent = paragraph;
+        newParagraph.setAttribute("class", "about__content");
+        about.appendChild(newParagraph);
+      }
+    )
+  }
+
+  function createJourneyCards() {
+    const journeyElement = document.querySelector(".journey__content");
+    journeyCardsContent.forEach(
+      function(cardContent) {
+        const newCard = document.createElement("div");
+        newCard.setAttribute("class", "journey__card");
+
+        const newTitle = document.createElement("h3");
+        newTitle.setAttribute("class", "journey__title");
+        newTitle.textContent = cardContent.title;
+        newCard.appendChild(newTitle);
+
+        const newDescription = document.createElement("p");
+        newDescription.innerHTML = cardContent.description;
+
+        if (cardContent.link) {
+          const newLink = document.createElement("a");
+          newLink.setAttribute("class", "journey__link");
+          newLink.setAttribute("href", cardContent.link);
+          newCard.appendChild(newLink);
+          newLink.appendChild(newDescription);
+        }
+        else {
+          newCard.appendChild(newDescription);
+        }
+
+
+        journeyElement.appendChild(newCard);
+      }
+    )
+  }
+
+  function createProjectCards() {
+    const projectElement = document.querySelector(".projects__container");
+    projectCardsContent.forEach(
+      function(cardContent) {
+        const newCard = document.createElement("div");
+        newCard.setAttribute("class", "projects__item");
+
+        const newLink = document.createElement("a");
+        newLink.setAttribute("target", "_blank");
+        newLink.setAttribute("class", "projects__card");
+        newLink.setAttribute("href", cardContent.link);
+        newCard.appendChild(newLink);
+
+        const newImage = document.createElement("img");
+        newImage.setAttribute("class", "projects__illustration");
+        newImage.setAttribute("src", cardContent.image);
+        newImage.setAttribute("alt", "Illustration projet " + cardContent.title);
+        newLink.appendChild(newImage);
+
+        const newTitle = document.createElement("h3");
+        newTitle.setAttribute("class", "projects__title");
+        newTitle.textContent = cardContent.title;
+        newLink.appendChild(newTitle);
+
+        const newDescription = document.createElement("p");
+        newDescription.setAttribute("class", "projects__description");
+        newDescription.innerHTML = cardContent.description;
+        newCard.appendChild(newDescription);
+
+        if (cardContent.githubLink) {
+          const newGithubLink = document.createElement("a");
+          newGithubLink.setAttribute("target", "_blank");
+          newGithubLink.setAttribute("class", "projects__github");
+          newGithubLink.setAttribute("href", cardContent.githubLink);
+          newGithubLink.innerText = "Voir sur Github";
+          newDescription.innerHTML += "<br>";
+          newDescription.appendChild(newGithubLink);
+        }
+
+
+        projectElement.appendChild(newCard);
+      }
+    )
+  }
+
+  createAboutMe();
+  createJourneyCards();
+  createProjectCards();
 }
 
 document.addEventListener('DOMContentLoaded', init);
---
+++ content.js
@@ -0,0 +1,71 @@
+const journeyCardsContent = [
+  {
+    title: "Accusamus, minus neque ea ab sapiente incidunt laboriosam",
+    link: "#projects",
+    description: "um totam nemo sunt facilis inventore commodi et libero voluptas."
+  },
+  {
+    title: "Accusamus, minus neque ea ab sapiente incidunt laboriosam",
+    link: "#projects",
+    description: "um totam nemo sunt facilis inventore commodi et libero voluptas."
+  },
+  {
+    title: "Accusamus, minus neque ea ab sapiente incidunt laboriosam",
+    link: "#projects",
+    description: "um totam nemo sunt facilis inventore commodi et libero voluptas."
+  },
+  {
+    title: "Accusamus, minus neque ea ab sapiente incidunt laboriosam",
+    link: "#projects",
+    description: "um totam nemo sunt facilis inventore commodi et libero voluptas."
+  },
+  {
+    title: "Accusamus, minus neque ea ab sapiente incidunt laboriosam",
+    link: "#projects",
+    description: "um totam nemo sunt facilis inventore commodi et libero voluptas."
+  },,
+  {
+    title: "Accusamus, minus neque ea ab sapiente incidunt laboriosam",
+    link: "#projects",
+    description: "um totam nemo sunt facilis inventore commodi et libero voluptas."
+  },
+  {
+    title: "Accusamus, minus neque ea ab sapiente incidunt laboriosam",
+    link: "#projects",
+    description: "um totam nemo sunt facilis inventore commodi et libero voluptas."
+  },
+  {
+    title: "Accusamus, minus neque ea ab sapiente incidunt laboriosam",
+    link: "#projects",
+    description: "um totam nemo sunt facilis inventore commodi et libero voluptas."
+  },
+  // {
+  //   title: "",
+  //   link: "",
+  //   description: ""
+  // },
+];
+
+const projectCardsContent = [
+  {
+    title: "Totam",
+    link: "#introduction",
+    image: "https://cdn.pixabay.com/photo/2025/03/07/13/12/flower-9453063_1280.jpg",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium tempora beatae corporis perferendis ratione ea consequatur quod eaque molestiae magni iusto impedit illum, consectetur explicabo recusandae deleniti facere assumenda? <br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium tempora beatae corporis perferendis ratione ea consequatur quod eaque molestiae magni iusto impedit illum, consectetur explicabo recusandae deleniti facere assumenda?"
+  },
+  {
+    title: "Inventore",
+    link: "#introduction",
+    githubLink: "https://github.com",
+    image: "https://cdn.pixabay.com/photo/2025/03/07/13/12/flower-9453063_1280.jpg",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium tempora beatae corporis perferendis ratione ea consequatur quod eaque molestiae magni iusto impedit illum, consectetur explicabo recusandae deleniti facere assumenda? <br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium tempora beatae corporis perferendis ratione ea consequatur quod eaque molestiae magni iusto impedit illum, consectetur explicabo recusandae deleniti facere assumenda?"
+  },
+  {
+    title: "Laboriosam",
+    link: "#introduction",
+    githubLink: "https://github.com",
+    image: "https://cdn.pixabay.com/photo/2025/03/07/13/12/flower-9453063_1280.jpg",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium tempora beatae corporis perferendis ratione ea consequatur quod eaque molestiae magni iusto impedit illum, consectetur explicabo recusandae deleniti facere assumenda? <br> Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam accusantium tempora beatae corporis perferendis ratione ea consequatur quod eaque molestiae magni iusto impedit illum, consectetur explicabo recusandae deleniti facere assumenda?"
+  },
+
+];
